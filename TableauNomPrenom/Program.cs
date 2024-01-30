ebc641389bcd456cfbfc10df6273d7a1d90e1aa9
necati han <han.necati@gmail.com>
2024-01-30 15:28:03 +0100

    Display a list of name from a file

--- TableauNomPrenom/Program.cs
+++ TableauNomPrenom/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace TableauNomPrenom
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TableauNomPrenom
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+            List<string> listePrenom = new List<string>();
+            StreamReader nameFile = new StreamReader("/Users/necatihan/RiderProjects/TableauNomPrenom/Name");
+            string line;
+            while ((line = nameFile.ReadLine()) != null)
+            {
+                
+                listePrenom.Add(line);
+            }
+
+            foreach (string name in listePrenom)
+            {
+                Console.WriteLine(name);
+            }
+        }
     }
-  }
 }
